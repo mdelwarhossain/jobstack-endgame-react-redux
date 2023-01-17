ca4fa96041738b708d39e2d2273a44b46d270615
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2023-01-18 00:19:08 +0600

    statistics

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Statistics from '../Statistics/Statistics';
 
 const Home = () => {
     return (
         <div>
-            home
+            <Statistics/>
         </div>
     );
 };
