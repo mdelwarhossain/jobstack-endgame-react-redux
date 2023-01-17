7c3a1d162f6b2bfab7116f6112b41fa46ed58ceb
MahabubImamEfaz <imtiyaz.efaz@gmail.com>
2023-01-18 01:03:42 +0600

    newsfeed created

--- client/src/Routes/Routes.js
+++ client/src/Routes/Routes.js
@@ -1,17 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Outlet/Main";
 import Home from "../Pages/Home/Home";
+import NewsFeed from "../Pages/NewsFeed/NewsFeed";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Main></Main>,
-        children: [
-            {
-                path:'/',
-                element:<Home></Home>
-            }
-        ]
-    }
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/newsfeed",
+        element: <NewsFeed></NewsFeed>,
+      },
+    ],
+  },
 ]);
 export default router;
