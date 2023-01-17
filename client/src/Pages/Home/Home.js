97fe88953000fe659f7742f652ea9e04fe1fd5eb
MRPrio <maksudurprio.mp@gmail.com>
2023-01-18 00:38:52 +0600

    banner added

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,18 +1,10 @@
 import React from 'react';
-<<<<<<< HEAD
 import Banner from './Banner/Banner';
-=======
-import Statistics from '../Statistics/Statistics';
->>>>>>> ca4fa96041738b708d39e2d2273a44b46d270615
 
 const Home = () => {
     return (
         <div>
-<<<<<<< HEAD
           <Banner></Banner>
-=======
-            <Statistics/>
->>>>>>> ca4fa96041738b708d39e2d2273a44b46d270615
         </div>
     );
 };
