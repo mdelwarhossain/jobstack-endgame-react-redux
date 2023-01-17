00a0220bcdb30db4cea47c5e32acf42938656722
MRPrio <maksudurprio.mp@gmail.com>
2023-01-18 00:46:15 +0600

    about section added

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Statistics from '../Statistics/Statistics';
+import About from './AboutUs/About';
 import Banner from './Banner/Banner';
 
 const Home = () => {
     return (
         <div>
           <Banner></Banner>
+          <About></About>
+          <Statistics></Statistics>
         </div>
     );
 };
