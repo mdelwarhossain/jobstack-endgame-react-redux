5058b8200fd95ff0f31e65062810e59aec5f530c
MRPrio <maksudurprio.mp@gmail.com>
2023-01-18 02:04:14 +0600

    newsfeed grid added

---
+++ client/src/Pages/NewsFeed/LeftSideCard/LeftSideCard.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import timeBg from '../../../assest/images/timebg.jpg'
+import clockbg from '../../../assest/images/clock.jpg'
+
+const LeftSideCard = () => {
+  const [dateState, setDateState] = useState(new Date());
+
+  useEffect(() => {
+    setInterval(() => setDateState(new Date()), 30000);
+  }, []);
+
+  return (
+    // <div>
+    //    <div>
+    //    <p>
+    //       {' '}
+    //       {dateState.toLocaleDateString('en-GB', {
+    //          day: 'numeric',
+    //          month: 'short',
+    //          year: 'numeric',
+    //       })}
+    //     </p>
+    //    </div>
+    //    <div>
+    //    {dateState.toLocaleString('en-US', {
+    //         hour: 'numeric',
+    //         minute: 'numeric',
+    //         hour12: true,
+    //     })}
+    //    </div>
+    // </div>
+
+    <div
+      className="hero"
+      style={{ backgroundImage: `url(${clockbg})`, height: "250px"}}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-2xl font-bold">
+          {dateState.toLocaleString('en-US', {
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true,
+        })}
+          </h1>
+          <p className="mb-5">
+          {dateState.toLocaleDateString('en-GB', {
+             day: 'numeric',
+             month: 'short',
+             year: 'numeric',
+          })}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LeftSideCard;
--- client/src/Pages/NewsFeed/NewsFeed.js
+++ client/src/Pages/NewsFeed/NewsFeed.js
@@ -1,9 +1,19 @@
 import React from "react";
+import LeftSideCard from "./LeftSideCard/LeftSideCard";
+import './NewsFeed.css'
 
 const NewsFeed = () => {
   return (
-    <div>
-      <h1>NewsFeed</h1>
+    <div className="newsFeed">
+     <div className="bg-slate-200">
+      <LeftSideCard></LeftSideCard>
+     </div>
+     <div className="bg-green-200">
+      <p>Newsfeed</p>
+     </div>
+     <div className="bg-red-200">
+      <p>rightSidebar</p>
+     </div>
     </div>
   );
 };
