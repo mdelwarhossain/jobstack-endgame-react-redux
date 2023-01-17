6cc9034f4ea366f0c54dfd1339c6f908841c48d3
SADIA AFSANA <afsanasadia2022@gmail.com>
2023-01-18 01:48:32 +0600

    login signup

--- client/src/Routes/Routes.js
+++ client/src/Routes/Routes.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Outlet/Main";
 import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/SignUp/SignUp";
 
 const router = createBrowserRouter([
     {
@@ -8,9 +10,18 @@ const router = createBrowserRouter([
         element: <Main></Main>,
         children: [
             {
-                path:'/',
-                element:<Home></Home>
-            }
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+
+            {
+                path: '/signup',
+                element: <SignUp></SignUp>
+            },
         ]
     }
 ]);
