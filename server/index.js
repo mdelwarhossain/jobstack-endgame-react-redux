4ee8e5e2989448b7181b9becf1e11b2be65df9db
MahabubImamEfaz <imtiyaz.efaz@gmail.com>
2023-01-18 16:50:30 +0600

    post sending to DB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -10,10 +10,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ziqz1ow.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 // verify jwt
 // function verifyJWT(req, res, next) {
@@ -36,34 +38,43 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 // }
 
 async function run() {
-    try {
-        const usersCollection = client.db('jobStack').collection('users');
+  try {
+    const usersCollection = client.db("jobStack").collection("users");
+    const postsCollection = client.db("jobStack").collection("posts");
 
-        
-        
+    //get the posts from Createpost components
+    app.post("/posts", async (req, res) => {
+      const post = req.body;
+      const result = await postsCollection.insertOne(post);
+      res.send(result);
+    });
 
+    //get all the posts from database. show it in {NewsFeedposts} Component
+    app.get("/allposts", async (req, res) => {
+      const query = {};
+      const cursor = postsCollection.find(query).sort({ _id: -1 });
+      const posts = await cursor.toArray();
+      res.send(posts);
+    });
 
-        // generate jwt
-        // app.get('/jwt', async (req, res) => {
-        //     const email = req.query.email;
-        //     const query = { email: email };
-        //     const user = await usersCollection.findOne(query);
-        //     if (user) {
-        //         const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
-        //         return res.send({ accessToken: token });
-        //     }
-        //     res.status(403).send({ accessToken: '' })
-        // });
-
-    }
-    finally {
-
-    }
+    // generate jwt
+    // app.get('/jwt', async (req, res) => {
+    //     const email = req.query.email;
+    //     const query = { email: email };
+    //     const user = await usersCollection.findOne(query);
+    //     if (user) {
+    //         const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+    //         return res.send({ accessToken: token });
+    //     }
+    //     res.status(403).send({ accessToken: '' })
+    // });
+  } finally {
+  }
 }
 run().catch(console.log);
 
-app.get('/', async (req, res) => {
-    res.send('jobStack server is running');
-})
+app.get("/", async (req, res) => {
+  res.send("jobStack server is running");
+});
 
-app.listen(port, () => console.log(`jobStack running on ${port}`))
+app.listen(port, () => console.log(`jobStack running on ${port}`));
