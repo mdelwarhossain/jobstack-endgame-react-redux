6c38804c870174fdc44f9246656a9e8463881d51
SADIA AFSANA <afsanasadia2022@gmail.com>
2023-01-19 00:21:01 +0600

    mongo

--- server/index.js
+++ server/index.js
@@ -10,11 +10,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ziqz1ow.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://jobstack:yDpXnq0bUjaKatUw@cluster0.ziqz1ow.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
 });
 
 // verify jwt
@@ -38,43 +39,50 @@ const client = new MongoClient(uri, {
 // }
 
 async function run() {
-  try {
-    const usersCollection = client.db("jobStack").collection("users");
-    const postsCollection = client.db("jobStack").collection("posts");
+    try {
+        const usersCollection = client.db("jobStack").collection("users");
+        const postsCollection = client.db("jobStack").collection("posts");
 
-    //get the posts from Createpost components
-    app.post("/posts", async (req, res) => {
-      const post = req.body;
-      const result = await postsCollection.insertOne(post);
-      res.send(result);
-    });
+        //get the posts from Createpost components
+        app.post("/posts", async (req, res) => {
+            const post = req.body;
+            const result = await postsCollection.insertOne(post);
+            res.send(result);
+        });
 
-    //get all the posts from database. show it in {NewsFeedposts} Component
-    app.get("/allposts", async (req, res) => {
-      const query = {};
-      const cursor = postsCollection.find(query).sort({ _id: -1 });
-      const posts = await cursor.toArray();
-      res.send(posts);
-    });
+        //get all the posts from database. show it in {NewsFeedposts} Component
+        app.get("/allposts", async (req, res) => {
+            const query = {};
+            const cursor = postsCollection.find(query).sort({ _id: -1 });
+            const posts = await cursor.toArray();
+            res.send(posts);
+        });
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log(user)
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
 
-    // generate jwt
-    // app.get('/jwt', async (req, res) => {
-    //     const email = req.query.email;
-    //     const query = { email: email };
-    //     const user = await usersCollection.findOne(query);
-    //     if (user) {
-    //         const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
-    //         return res.send({ accessToken: token });
-    //     }
-    //     res.status(403).send({ accessToken: '' })
-    // });
-  } finally {
-  }
+        // generate jwt
+        // app.get('/jwt', async (req, res) => {
+        //     const email = req.query.email;
+        //     const query = { email: email };
+        //     const user = await usersCollection.findOne(query);
+        //     if (user) {
+        //         const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+        //         return res.send({ accessToken: token });
+        //     }
+        //     res.status(403).send({ accessToken: '' })
+        // });
+    }
+    finally {
+    }
 }
 run().catch(console.log);
 
 app.get("/", async (req, res) => {
-  res.send("jobStack server is running");
+    res.send("jobStack server is running");
 });
 
 app.listen(port, () => console.log(`jobStack running on ${port}`));
